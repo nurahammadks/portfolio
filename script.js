745fda9ff5d98f5fc75272f7f0609b5524d7bdb0
Nur Ahammad Khan Saykat <66208245+nurahammadks@users.noreply.github.com>
2020-06-10 20:09:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	$('.mobile-menu').click(function(){
 		$('.header-menu nav').fadeToggle(1000);
 	});
+	WOW = new WOW({
+		boxClass                  :       'wow',      //default
+		animateClass              :       'animated', //default
+		offset                    :       0,          //default
+		mobile                    :       true,       //default
+		live                      :       true        //default
+	})
+	WOW.init();
 });
